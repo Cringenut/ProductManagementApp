4c1908cb5b7badbee92039d5e55ccd23186efac1
Cringenut <ihnatbaikou@gmail.com>
2024-11-28 20:43:38 +0100

    Scroll to the top after adding new product

--- src/app/components/ProductGridComponent.js
+++ src/app/components/ProductGridComponent.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useLayoutEffect, useRef} from 'react';
 import ProductCard from "./ProductCardComponent";
 import '../../styles/ProuductGrid.css';
 import {ProductProvider, useProductContext} from "@/app/context/ProductContext";
@@ -6,10 +6,20 @@ import {ProductProvider, useProductContext} from "@/app/context/ProductContext";
 
 const ProductGrid = () => {
     const { filteredProducts } = useProductContext();
+    const gridRef = useRef(null);
+
+    useLayoutEffect(() => {
+        console.log("CHANGED")
+
+        if (gridRef.current) {
+            console.log("REF")
+            gridRef.current.scrollTo({ top: 0 });
+        }
+    }, [filteredProducts]); // Trigger whenever filteredProducts change
 
     return (
-        <div className="product-grid">
-            {filteredProducts.map((product, index) => (
+        <div className="product-grid" ref={gridRef}>
+            {filteredProducts.slice().reverse().map((product, index) => (
                 <ProductCard key={index} product={product} />
             ))}
         </div>
