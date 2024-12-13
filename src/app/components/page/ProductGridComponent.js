699cb39afb67646585a6ae47537df715087a8953
Cringenut <ihnatbaikou@gmail.com>
2024-12-13 13:59:25 +0100

    Default grid

--- src/app/components/page/ProductGridComponent.js
+++ src/app/components/page/ProductGridComponent.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { FixedSizeList as List } from 'react-window';
 import ProductCard from "./ProductCardComponent";
 import '../../../styles/ProuductGrid.css';
 
 const ProductGrid = ({ currentProducts }) => {
-    const Row = ({ index, style }) => (
-        <div style={style}>
-            <ProductCard product={currentProducts[index]} />
-        </div>
-    );
-
     return (
-        <List
-            height={500}
-            itemCount={currentProducts.length}
-            itemSize={250}
-            width={'100%'}
-        >
-            {Row}
-        </List>
+        <div className="product-grid">
+            {currentProducts.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
     );
 };
 
